c68e27c6fdeced87b7e7de2c78e5d3736b3c987e
Jennifer Woolsey <jennifer.a.woolsey@gmail.com>
2018-05-30 21:00:01 -0500

    Changes to design.js from mentor input

--- designs.js
+++ designs.js
@@ -1,24 +1,42 @@
 // Select color input
 var paint = document.getElementById('colorPicker').value;
+//var gridForm = $('#sizePicker');
+paint.addEventListener("switch", colors(), false);
 
 // Select size input
-var table = document.getElementById('pixelCanvas');
-var rows = document.getElementById('inputHeight').value;
-var cols = document.getElementById('inputWidth').value;
+
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit.addEventListener('click', makeGrid());
+$(document).ready($('#submit').on('click', function(event) {
+  //Prevent preventDefault
+  event.preventDefault();
+
+  //Fetch user size input
+  var rows = document.getElementById('inputHeight').value;
+  var cols = document.getElementById('inputWidth').value;
+
+  //make the grid
+  console.log(rows + cols);
+  makeGrid(rows, cols);
+}));
 
 function makeGrid(rows, cols) {
-
+  var table = document.getElementById('pixelCanvas');
 // Your code goes here!
-  var grid = document.createElement('table');
-  grid.className = 'grid';
-  for (let r of rows) {
-    var tr = grid.appendChild(document.createElement('tr'));
-    for (let c of cols) {
-      var td = tr.appendChild(document.createElement('td'));
+  for (var r = 1; r <= rows; r++) {
+    var tr = table.append(document.createElement('tr'));
+    for (var c = 1; c <= cols; c++) {
+      var td = tr.append(document.createElement('td'));
     }
   }
-  return grid;
+  document.body.appendChild(table);
+}
+
+
+
+// Select Colors
+function colors() {
+  let paintColor = paint.value;
+  grid.style.color = paintColor;
+  return paintColor;
 }
